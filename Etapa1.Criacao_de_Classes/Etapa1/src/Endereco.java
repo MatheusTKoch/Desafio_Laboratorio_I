7f2696dd71a2ab5411675a1df5c34ffdd5dd3a96
Matheus <matheustrilhakoch@hotmail.com>
2024-05-22 21:53:57 -0300

    Finalizando endereco

--- Etapa1.Criacao_de_Classes/Etapa1/src/Endereco.java
+++ Etapa1.Criacao_de_Classes/Etapa1/src/Endereco.java
@@ -39,7 +39,35 @@ public class Endereco {
         return complemento;
     }
 
-    public void setNomeDaRua() {
+    public void setNomeDaRua(String nomeDaRua) {
         this.nomeDaRua = nomeDaRua;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String toString() {
+        return "Endereco compleo: Rua " + nomeDaRua + ", numero" + numero + ", " + cidade + " - " + estado + ", CEP " + cep + ", " + pais +". Complemento: " + complemento ; 
     }
 }
