794fa1e2cb4a496ff7b4a432ea5f38a32c0479ab
Matheus <matheustrilhakoch@hotmail.com>
2024-05-25 15:53:55 -0300

    Testando input

--- Matheus_Trilha_Koch_GR96002-00000/Etapa1/src/Principal.java
+++ Matheus_Trilha_Koch_GR96002-00000/Etapa1/src/Principal.java
@@ -4,14 +4,29 @@ import java.util.InputMismatchException;
 public class Principal {
     public static void main(String[] args) throws Exception {
         System.out.println("(1) criar uma loja");
-        System.out.println("(2) criar um produto");
+        System.out.println("(1) criar um produto");
         System.out.println("(3) sair");
 
         Scanner input = new Scanner(System.in);
         try {
             int num = input.nextInt();
+            if (num == 1) {
+                System.out.println("Criação da loja");
+            } else if (num == 2) {
+                System.out.println("Criação do produto");
+            } else if (num == 3) {
+                input.close();
+            } else {
+                System.out.println("(1) criar uma loja");
+                System.out.println("(1) criar um produto");
+                System.out.println("(3) sair");
+            }
         } catch (InputMismatchException e) {
             System.out.println("Opção  inválida");
+            System.out.println("(1) criar uma loja");
+            System.out.println("(1) criar um produto");
+            System.out.println("(3) sair");
         }
+        
     }
 }
