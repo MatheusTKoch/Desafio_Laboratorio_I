752bb123b20f25463ec761c0799f87d1dae4118e
Matheus <matheustrilhakoch@hotmail.com>
2024-05-25 15:18:42 -0300

    Criando classe Principal e menu

---
+++ Matheus_Trilha_Koch_GR96002-00000/Etapa1/src/Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+    public static void main(String[] args) throws Exception {
+        System.out.println("(1) criar uma loja");
+        System.out.println("(2) criar um produto");
+        System.out.println("(3) sair");
+    }
+}
