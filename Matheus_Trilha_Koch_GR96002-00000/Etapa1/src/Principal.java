de61180f0f25baa07414f548b750f0ace12a6408
Matheus <matheustrilhakoch@hotmail.com>
2024-05-25 15:37:38 -0300

    Testando Inputs

--- Matheus_Trilha_Koch_GR96002-00000/Etapa1/src/Principal.java
+++ Matheus_Trilha_Koch_GR96002-00000/Etapa1/src/Principal.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class Principal {
     public static void main(String[] args) throws Exception {
         System.out.println("(1) criar uma loja");
         System.out.println("(2) criar um produto");
         System.out.println("(3) sair");
+
+        Scanner input = new Scanner(System.in);
+        try {
+            int num = input.nextInt();
+        } catch (InputMismatchException e) {
+            System.out.println("Opção  inválida");
+        }
     }
 }
