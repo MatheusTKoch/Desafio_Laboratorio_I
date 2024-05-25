e2ec66d55d2ab91c64a1ef60cf2857d00ff8e00f
Matheus <matheustrilhakoch@hotmail.com>
2024-05-25 14:47:26 -0300

    Ajustando Produto

--- Matheus_Trilha_Koch_GR96002-00000/Etapa1/src/Produto.java
+++ Matheus_Trilha_Koch_GR96002-00000/Etapa1/src/Produto.java
@@ -1,10 +1,12 @@
 public class Produto {
     private String nome;
     private double preco;
+    private Data dataValidade;
 
-    public Produto(String nome, double preco) {
+    public Produto(String nome, double preco, Data dataValidade) {
         this.nome = nome;
         this.preco = preco;
+        this.dataValidade = dataValidade;
     }
 
     public String getNome() {
@@ -15,12 +17,20 @@ public class Produto {
         return preco;
     }
 
+    public Data dataValidade() {
+        return dataValidade;
+    }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
 
     public void setPreco(double preco) {
         this.preco = preco;
+    }
+
+    public void setDataValidade(Data dataValidade) {
+        this.dataValidade = dataValidade;
     }
 
     public String toString() {
