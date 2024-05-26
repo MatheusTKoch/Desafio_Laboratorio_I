99fa15fd554c3537d1fc95560452383b5b534ad4
Matheus <matheustrilhakoch@hotmail.com>
2024-05-25 22:47:25 -0300

    Ajustando loop Loja

--- Matheus_Trilha_Koch_GR96002-00000/Etapa1/src/Principal.java
+++ Matheus_Trilha_Koch_GR96002-00000/Etapa1/src/Principal.java
@@ -1,32 +1,38 @@
 import java.util.Scanner;
-import java.util.InputMismatchException;
 
 public class Principal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("(1) criar uma loja");
-        System.out.println("(1) criar um produto");
-        System.out.println("(3) sair");
-
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        try {
-            int num = input.nextInt();
-            if (num == 1) {
-                System.out.println("Criação da loja");
-            } else if (num == 2) {
-                System.out.println("Criação do produto");
-            } else if (num == 3) {
-                input.close();
-            } else {
-                System.out.println("(1) criar uma loja");
-                System.out.println("(1) criar um produto");
-                System.out.println("(3) sair");
-            }
-        } catch (InputMismatchException e) {
-            System.out.println("Opção  inválida");
+        int opcao = 0;
+
+        while (opcao !=3) {
             System.out.println("(1) criar uma loja");
-            System.out.println("(1) criar um produto");
+            System.out.println("(2) criar um produto");
             System.out.println("(3) sair");
-        }
         
+            if(input.hasNextInt()) {
+                opcao = input.nextInt();
+            
+            switch (opcao) {
+                case 1:
+                    System.out.println("Criação da loja");
+                    System.out.println("Nome da Loja: ");
+                    break;
+                case 2:
+                    System.out.println("Criação do produto");
+                    break;
+                case 3:
+                    System.out.println("Saindo");
+                    break;
+                default:
+                    System.out.println("Opção  inválida");   
+                }
+            } else {
+                System.out.println("Opção  inválida");
+                input.next();
+            }
+            System.out.println(); 
+        }
+        input.close(); 
     }
 }
