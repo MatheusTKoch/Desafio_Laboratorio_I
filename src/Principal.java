191e3aac33a09de61f714c77b1cf783baaa9bae2
Matheus <matheustrilhakoch@hotmail.com>
2024-05-29 20:32:04 -0300

    Adicionando objetos de instancia da loja criada

--- src/Principal.java
+++ src/Principal.java
@@ -8,10 +8,7 @@ public class Principal {
         //usar classe Teclado para refatorar o codigo abaixo ao pedir os inputs
 
         while (opcao !=3) {
-            opcao = Teclado.leInt("(1) criar uma loja (2) criar um produto (3) sair");
-        
-            if(scanner.hasNextInt()) {
-                opcao = scanner.nextInt();
+            opcao = Teclado.leInt("(1) criar uma loja (2) criar um produto (3) sair  -  ");
             
             switch (opcao) {
                 case 1:
@@ -23,9 +20,15 @@ public class Principal {
                     String cidade = Teclado.leString("Cidade: ");
                     String estado = Teclado.leString("Estado: ");
                     String pais = Teclado.leString("Pais: ");
-                    String numero = Teclado.leString("Numero: ");
                     String cep = Teclado.leString("CEP: ");
+                    String numero = Teclado.leString("Numero: ");
                     String complemento = Teclado.leString("Complemento: ");
+                    int dia = Teclado.leInt("Dia da Fundação da loja: ");
+                    int mes = Teclado.leInt("Mes da Fundação da loja: ");
+                    int ano = Teclado.leInt("Ano da Fundação da loja ");
+                    Data dataFund = new Data(dia, mes, ano);
+                    Endereco endereco = new Endereco(nomeRua, cidade, estado, pais, cep, numero, complemento);
+                    Loja loja = new Loja(nome, quantFunc, salBase, endereco, dataFund);
                     System.out.println("Loja cadastrada! Por favor cadastre um produto em seguida");
                     break;
                 case 2:
@@ -37,12 +40,8 @@ public class Principal {
                 default:
                     System.out.println("Opção  inválida");   
                 }
-            } else {
-                System.out.println("Opção  inválida");
-                scanner.next();
             }
-            System.out.println(); 
-        }
+        
         scanner.close(); 
     }
 }
