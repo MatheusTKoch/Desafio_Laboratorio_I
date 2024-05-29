5fee02d0606750d512c4f29425671d8e3667e5eb
Matheus <matheustrilhakoch@hotmail.com>
2024-05-29 20:22:21 -0300

    Utilizando classe Teclado

--- src/Principal.java
+++ src/Principal.java
@@ -8,9 +8,7 @@ public class Principal {
         //usar classe Teclado para refatorar o codigo abaixo ao pedir os inputs
 
         while (opcao !=3) {
-            System.out.println("(1) criar uma loja");
-            System.out.println("(2) criar um produto");
-            System.out.println("(3) sair");
+            opcao = Teclado.leInt("(1) criar uma loja (2) criar um produto (3) sair");
         
             if(scanner.hasNextInt()) {
                 opcao = scanner.nextInt();
@@ -18,26 +16,16 @@ public class Principal {
             switch (opcao) {
                 case 1:
                     System.out.println("Criação da loja");
-                    System.out.println("Nome da Loja: ");
-                    String nome = scanner.next();
-                    System.out.println("Quantidade de funcionarios: ");
-                    int quantFunc = scanner.nextInt();
-                    System.out.println("Salario base dos funcionarios: ");
-                    double salBase = scanner.nextDouble();
-                    System.out.println("Nome da Rua: ");
-                    String nomeRua = scanner.next();
-                    System.out.println("Cidade: ");
-                    String cidade = scanner.next();
-                    System.out.println("Estado: ");
-                    String estado = scanner.next();
-                    System.out.println("Pais: ");
-                    String pais = scanner.next();
-                    System.out.println("Numero: ");
-                    String numero = scanner.next();
-                    System.out.println("CEP: ");
-                    String cep = scanner.next();
-                    System.out.println("Complemento: ");
-                    String complemento = scanner.next();
+                    String nome = Teclado.leString("Nome da Loja: ");
+                    int quantFunc = Teclado.leInt("Quantidade de funcionarios: ");
+                    double salBase = Teclado.leDouble("Salario base dos funcionarios: ");
+                    String nomeRua = Teclado.leString("Nome da Rua: ");
+                    String cidade = Teclado.leString("Cidade: ");
+                    String estado = Teclado.leString("Estado: ");
+                    String pais = Teclado.leString("Pais: ");
+                    String numero = Teclado.leString("Numero: ");
+                    String cep = Teclado.leString("CEP: ");
+                    String complemento = Teclado.leString("Complemento: ");
                     System.out.println("Loja cadastrada! Por favor cadastre um produto em seguida");
                     break;
                 case 2:
