e55ae538f6af8c6e385e8080160614b7af4e6dff
Matheus <matheustrilhakoch@hotmail.com>
2024-05-30 10:30:04 -0300

    Ajustando anoBissexto

--- src/Data.java
+++ src/Data.java
@@ -45,7 +45,7 @@ public class Data {
     }
 
     public boolean verificaAnoBissexto() {
-        if (ano % 4 == 0 || ano % 100 == 0 && ano % 400 == 0) {
+        if (ano % 100 != 0 && ano % 4 == 0 || ano % 100 == 0 && ano % 400 == 0) {
             return true;
         } else {
             return false;
