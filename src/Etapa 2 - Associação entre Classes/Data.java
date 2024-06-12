9f2560ecd1a52059d6073b978f9ba68e46d66899
Matheus <matheustrilhakoch@hotmail.com>
2024-06-11 21:54:39 -0300

    Adicionada Etapa 2 separadamente

---
+++ src/Etapa 2 - Associação entre Classes/Data.java
@@ -0,0 +1,54 @@
+public class Data {
+    private int dia;
+    private int mes;
+    private int ano;
+
+    public Data(int dia, int mes, int ano) {
+        if (dia == 29 && mes == 2 && ano % 100 != 0 && ano % 4 == 0 || dia == 29 && mes == 2 && ano % 100 == 0 && ano % 400 == 0) {
+            System.out.println("Data inválida!");
+            this.dia = 1;
+            this.mes = 1;
+            this.ano = 2000;
+        } else {
+            this.dia = dia;
+            this.mes = mes;
+            this.ano = ano;
+        }
+    }
+
+    public int getDia() {
+        return dia;
+    }
+
+    public int getMes() {
+        return mes;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setDia(int dia) {
+        this.dia = dia;
+    }
+
+    public void setMes(int mes) {
+        this.mes = mes;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public String toString() {
+        return dia + "/" + mes + "/" + ano;
+    }
+
+    public boolean verificaAnoBissexto() {
+        if (ano % 100 != 0 && ano % 4 == 0 || ano % 100 == 0 && ano % 400 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/Etapa 2 - Associação entre Classes/Principal.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcao = 0;
+        boolean lojaCadastrada = false;
+        Data validadePadrao = new Data(20, 10, 2023);
+        Data dataFund = new Data(0, 0, 0);
+        Endereco enderecoLoja =new Endereco(null, null, null, null, null, null, null);
+        Loja loja = new Loja(null, 0, enderecoLoja, dataFund);
+
+        while (opcao !=3) {
+            opcao = Teclado.leInt("(1) criar uma loja (2) criar um produto (3) sair  -  ");
+            switch (opcao) {
+                case 1:
+                    System.out.println("Criação da loja");
+                    String nomeLoja = Teclado.leString("Nome da Loja: ");
+                    int quantFuncLoja = Teclado.leInt("Quantidade de funcionarios: ");
+                    double salBaseLoja = Teclado.leDouble("Salario base dos funcionarios: ");
+                    String nomeRua = Teclado.leString("Nome da Rua: ");
+                    String cidade = Teclado.leString("Cidade: ");
+                    String estado = Teclado.leString("Estado: ");
+                    String pais = Teclado.leString("Pais: ");
+                    String cep = Teclado.leString("CEP: ");
+                    String numero = Teclado.leString("Numero: ");
+                    String complemento = Teclado.leString("Complemento: ");
+                    int diaFund = Teclado.leInt("Dia da Fundação da loja: ");
+                    int mesFund = Teclado.leInt("Mes da Fundação da loja: ");
+                    int anoFund = Teclado.leInt("Ano da Fundação da loja ");
+                    dataFund = new Data(diaFund, mesFund, anoFund);
+                    enderecoLoja = new Endereco(nomeRua, cidade, estado, pais, cep, numero, complemento);
+                    loja = new Loja(nomeLoja, quantFuncLoja, salBaseLoja, enderecoLoja, dataFund);
+                    System.out.println("Loja cadastrada! Por favor cadastre um produto em seguida");
+                    lojaCadastrada = true;
+                    break;
+                case 2:
+                    System.out.println("Criação do produto");
+                    String nomeProd = Teclado.leString("Nome do Produto: ");
+                    double precoProd = Teclado.leDouble("Preco do produto: ");
+                    int diaVal = Teclado.leInt("Dia da validade: ");
+                    int mesVal = Teclado.leInt("Mes da validade: ");
+                    int anoVal = Teclado.leInt("Ano da validade: ");
+                    Data dataValidade = new Data(diaVal, mesVal, anoVal);
+                    Produto produto = new Produto(nomeProd, precoProd, dataValidade);
+                    System.out.println("Produto Cadastrado!");
+                    if (lojaCadastrada) {
+                        if (produto.estaVencido(validadePadrao)) {
+                            System.out.println("PRODUTO VENCIDO");
+                        } else {
+                            System.out.println("PRODUTO NÃO VENCIDO");
+                        }
+                        System.out.println(loja.toString());
+                        System.out.println(enderecoLoja.toString());
+                        System.out.print("Data de fundação: " + dataFund.toString());
+                        opcao = 3;
+                    }
+                    break;
+                case 3:
+                    System.out.println("Saindo");
+                    break;
+                default:
+                    System.out.println("Opção  inválida");   
+                }
+            }
+        scanner.close(); 
+    }
+}
